394f77ef8e8501c1c43af15ab1731870fd9c52c5
dali <dalissj1@gmail.com>
2021-02-11 18:33:57 +0100

    heroku commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express =require("express");
 const bodyParser= require("body-parser");
 const request = require("request");
 const https =require("https");
-const port = 3000; 
+const port = process.env.PORT || 3000; 
 
 const app = express(); 
 app.use(bodyParser.urlencoded({extended:true}));
